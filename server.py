b6a917dedf048349f4986515119cd0cd67023cce
Mingqian Ye <mye@magicleap.com>
2018-03-31 02:14:56 -0400

    work

--- server.py
+++ server.py
@@ -10,10 +10,10 @@ def process(item):
     return {"title": item["title"], "content": item["content"]}
 
 @app.route("/news")
-@cache.cached(timeout=60)
+@cache.cached(timeout=300)
 def hello():
-    result = ts.get_latest_news(top=20,show_content=True).to_dict('records')
-    return jsonify(list(map(process, result)))
+    result = ts.get_latest_news(top=50,show_content=True).to_dict('records')
+    return jsonify({"news": list(map(process, result))})
 
 
 if __name__ == '__main__':
